5744160f2ef50f60645f8407d4ba67be8760a3c5
Priyanshu Raj <priyanshu.rajchauhan136315@gmail.com>
2026-09-11 03:06:05 +0530

    Tools added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Langchain-MultiAgent-Research-System
+
---
+++ main.py
@@ -0,0 +1,3 @@
+from src.tools.tools import web_search,scrape_url
+output =scrape_url.invoke({"url":"https://www.artificialintelligence-news.com"})
+print(output)
